16e7288252c215bc29df094594df5f9fa6f72e8f
20YN04 <Yentl.nerinckx@icloud.com>
2026-06-05 13:34:53 +0200

    Fix import order in AdminPanelProvider by moving LocalFontProvider to the correct position

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,11 +2,11 @@
 
 namespace App\Providers\Filament;
 
+use Filament\FontProviders\LocalFontProvider;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Filament\FontProviders\LocalFontProvider;
 use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
