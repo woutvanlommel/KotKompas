92d9575ec091e9c68aac36fe10c9e174cdcb527e
AMicciche <alessiomicciche@outlook.com>
2026-06-09 11:02:11 +0200

    setup of the message event for reverb

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Message;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessageSent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(public Message $message)
+    {
+        //
+    }
+
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('conversation.' . $this->message->conversation_id),
+        ];
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'id'              => $this->message->id,
+            'body'            => $this->message->body,
+            'sender_id'       => $this->message->sender_id,
+            'conversation_id' => $this->message->conversation_id,
+            'created_at'      => $this->message->created_at->toISOString(),
+        ];
+    }
+}
