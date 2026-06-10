23b236987342a2c6f69bf89d67eb7b9338c9c5a2
woutvanlommel <woutvanlommel@icloud.com>
2026-06-10 16:12:07 +0200

    fix: correct instantiation of HtmlSanitizerConfig in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->singleton(HtmlSanitizer::class, function () {
-            $config = (new HtmlSanitizerConfig())
+            $config = (new HtmlSanitizerConfig)
                 ->allowSafeElements();
 
             return new HtmlSanitizer($config);
