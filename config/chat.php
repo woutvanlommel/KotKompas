742c438f399f75d53c6d613b0d16185b90687128
AMicciche <alessiomicciche@outlook.com>
2026-06-15 14:59:58 +0200

    Added the ability to blacklist words for the chat. Users enter a prohibited word, it gets checked before broadcast and if not allowed show a toast.

---
+++ config/chat.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Chat Message Blacklist
+    |--------------------------------------------------------------------------
+    |
+    | Words listed here will block a message from being sent. The check is
+    | case-insensitive and matches whole words only.
+    |
+    */
+
+    'blacklist' => [
+        // Dutch
+        'kut',
+        'klootzak',
+        'lul',
+        'eikel',
+        'hoer',
+        'slet',
+        'godverdomme',
+        'godver',
+        'kanker',
+        'tyfus',
+        'tering',
+        'pest',
+        'mongool',
+        'debiel',
+        'idioot',
+        'sukkel',
+        'kankerlul',
+        'kankerhond',
+
+        // English
+        'fuck',
+        'shit',
+        'bitch',
+        'asshole',
+        'bastard',
+        'cunt',
+        'dick',
+        'prick',
+        'whore',
+        'slut',
+        'motherfucker',
+        'nigger',
+    ],
+
+];
